91bb8c566abc605c17b1632b4a70281ba671b2d2
Regie Marzan <lonebeginner@gmail.com>
2023-04-29 15:20:50 +0800

    add color property

--- includes/Construct.php
+++ includes/Construct.php
@@ -1,16 +1,29 @@
 <?php
 class App{
     public $model;
+    public $color;
     
     function setModel($model){
         $this->model=$model;
+    }
+
+    
+    function setColor($color){
+        $this->model=$color;
     }
 
     function getModel(){
         return $this->model;
     }
 
+    function getColor(){
+        return $this->color;
+    }
+
 }
 
 $CM = new App();
 $CM->setModel('CM');
+
+$golden = new App();
+$CM->setColor('golden');
